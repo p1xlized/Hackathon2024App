8065c69ff440912279a86a450f76cba328cfad70
e2134078 <e2134078@cmaisonneuve.qc.ca>
2024-01-28 00:38:16 -0500

    Ajout get pour billboard

--- screens/Billboard/billboard.js
+++ screens/Billboard/billboard.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, FlatList, ScrollView } from 'react-native';
 import EventCard from './billboardCard';
+import supabase from "../../lib/supabase";
 
-const eventsData = [
+/*const eventsData = [
   {
     id: '1',
     image: "https://m1.quebecormedia.com/emp/emp/AdobeStock_1425158875049fb30-d613-410d-97db-1a2c4d381c69_ORIGINAL.jpg?impolicy=crop-resize&x=0&y=485&w=3816&h=1573&width=1200",
@@ -21,21 +22,44 @@ const eventsData = [
     description: 'Join us for a live music concert this Saturday! Free entry for all!',
     postedBy: 'John Doe',
   },
-];
+];*/
 
 export default function Billboard() {
-  return (
-    <ScrollView>
-     <View>
-      <FlatList
-        data={eventsData}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <EventCard image={item.image} description={item.description} postedBy={item.postedBy} />
-        )}
-      />
-    </View>   
-    </ScrollView>
-    
-  );
+    const [eventsData, setEventsData] = useState(null)
+
+    async function getCards(){
+        try {
+            //only selects events published by unverified user
+            const { data, error } = await supabase.from('events').select('*, users(id, nom, prenom)').eq('user(verified)', 'false')
+            if (error) {
+            console.log(error);
+            } else {
+            console.log(data)
+            setEventsData(data)
+            }
+        } catch (error) {
+            console.error(error);
+            setError(error);
+        }
+    }
+
+    useEffect(() => {
+        console.log("get cards")
+        getCards()
+    },[])
+
+    return (
+        <ScrollView>
+        <View>
+        <FlatList
+            data={eventsData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+            <EventCard image={item.image} description={item.description} postedBy={item.published_by} />
+            )}
+        />
+        </View>   
+        </ScrollView>
+        
+    );
 };
