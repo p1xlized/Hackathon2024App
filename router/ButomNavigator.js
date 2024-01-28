097b58a6bcfbaf42c9af00ed810e4dc5320a2ef6
samy <e2072931@cmaisonneuve.qc.ca>
2024-01-27 20:00:31 -0500

    Sign up page <3

--- router/ButomNavigator.js
+++ router/ButomNavigator.js
@@ -2,7 +2,12 @@
 import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStack from '../screens/home';
+<<<<<<< Updated upstream
 import InfoStack from './InfoStack';
+=======
+import InfoStack from './InfoStack'
+import SignUp from "../screens/Signup";
+>>>>>>> Stashed changes
 import EventsStack from "./EventsStack";
 import Profil from '../screens/Profil/profil';
 import Login from '../screens/Profil/login';
@@ -15,6 +20,7 @@ const DrawerNavigator = () => {
     const [userId, setUserId] = useState(null);
 
     return (
+<<<<<<< Updated upstream
         <Tab.Navigator tabBar={props => <TabBar {...props} />}>
             <Tab.Screen name="Accueil" component={HomeStack} />
             <Tab.Screen name="Évènements" component={EventsStack} />
@@ -24,6 +30,13 @@ const DrawerNavigator = () => {
             ) : (
                 <Tab.Screen name="Login" component={Login} />
             )}
+=======
+        <Tab.Navigator>
+            <Tab.Screen name="Accueil" component={HomeStack} />
+            <Tab.Screen name="Évènements" component={EventsStack} />
+            <Tab.Screen name="Services" component={InfoStack} />
+            <Tab.Screen name="Profil" component={SignUp} />
+>>>>>>> Stashed changes
         </Tab.Navigator>
     );
 };
