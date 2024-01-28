77a7dcbff71d35415b4d5317f6f147781e1511c3
Nathan <nathan.decopain@gmail.com>
2024-01-27 23:39:29 -0500

    Continued events page

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,38 +1,66 @@
-import React from 'react'
-import {Avatar, Card, Text} from '@ui-kitten/components';
-import {StyleSheet, View} from "react-native";
-import FullWidthImage from "react-native-fullwidth-image";
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { Text } from '@ui-kitten/components';
+import FullWidthImage from 'react-native-fullwidth-image';
 
 const Header = () => {
     return (
         <View>
-            <FullWidthImage ratio={2/3} style={styles.cardImage} source={{
-                uri: "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            }}/>
+            <FullWidthImage
+                ratio={2 / 3}
+                style={styles.cardImage}
+                source={{
+                    uri:
+                        'https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                }}
+            />
         </View>
-    )
-}
-export const EventCard = (props) => {
+    );
+};
+
+export const EventCard = ({ eventName = '', locationName = '' }) => {
     return (
-        <Card header={Header} styles={styles.card}>
-                <Text category="h6">Heure du conte</Text>
-            <Text category={"c1"}>Bibliothèque Robert-Lussier</Text>
-            <Text category={"c2"}>19h00 - 20h00</Text>
-        </Card>
-    )
-}
+        <View style={styles.card}>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.cardImageLeft}
+                    source={{
+                        uri:
+                            'https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    }}
+                />
+            </View>
+            <View style={styles.textContainer}>
+                <Text category="s1">{eventName}</Text>
+                <Text category="c1">{locationName}</Text>
+                <Text category="c2">19h00 - 20h00</Text>
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     card: {
+        display: 'flex',
+        flexDirection: 'row', // Set flexDirection to 'row' for a horizontal layout
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        marginVertical: 8,
+    },
+    imageContainer: {
+        flex: 1, // Take 1/3 of the horizontal space
+        marginRight: 8, // Add some margin between image and text
+    },
+    textContainer: {
+        flex: 2, // Take 2/3 of the horizontal space
     },
     cardImage: {
-        resizeMode: "cover",
+        resizeMode: 'cover',
         marginTop: -16,
         marginBottom: 0,
     },
-    cardHeader: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between"
+    cardImageLeft: {
+        resizeMode: 'cover',
+        height: '100%', // Set height to '100%' to take the full height of the container
     },
-})
+});
--- screens/Events/Events.js
+++ screens/Events/Events.js
@@ -1,9 +1,28 @@
 import {EventCard} from "../../components/EventCard";
 import {FlatList, StyleSheet, TextInput, View, Button as ReactButton, TouchableOpacity} from "react-native";
 import {Button, Card, Icon, Layout, Modal, Text} from "@ui-kitten/components";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import supabase from "../../lib/supabase";
 
 export function Events({navigation}) {
+    const [error, setError] = useState(null);
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const {data, error} = await supabase
+                    .from("events")
+                    .select("*")
+
+                setEvents(await data)
+            } catch (error) {
+                console.error(error);
+                setError(error);
+            }
+        }
+        fetchEvents()
+    }, [])
 
     const [modalVisible, setModalVisible] = useState(false)
 
@@ -27,13 +46,24 @@ export function Events({navigation}) {
                         <Icon fill={"white"} style={styles.tagIcon} name={"close-outline"}></Icon>
                     </View>
                 </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.searchTag}>
+                        <Text category={"c1"} style={{color: "white"}}>un tag</Text>
+                        <Icon fill={"white"} style={styles.tagIcon} name={"close-outline"}></Icon>
+                    </View>
+                </TouchableOpacity>
             </Layout>
-
-            <FlatList style={styles.topContainer}
-                      data={[1, 2, 3]}
-                      renderItem={(item) => <EventCard></EventCard>}
-                      ItemSeparatorComponent={() => <View style={{height: 5}}/>}
-            />
+            { events &&
+                <View style={styles.eventsList}>
+                    <Text category={"h4"} style={styles.sectionDate}>27 janvier 2023</Text>
+                    <FlatList style={styles.topContainer}
+                              data={events}
+                              keyExtractor={(item, index) => item.id.toString() }
+                              renderItem={({item, index}) => <EventCard eventName={item.name} locationName={item.location}></EventCard>}
+                              ItemSeparatorComponent={() => <View style={{height: 5}}/>}
+                    />
+                </View>
+            }
 
             <Modal visible={modalVisible} backdropStyle={styles.backdrop}
                    onBackdropPress={() => setModalVisible(false)}>
@@ -47,7 +77,6 @@ export function Events({navigation}) {
 
 const styles = StyleSheet.create({
     topContainer: {
-        margin: 5,
         rowGap: 5
     },
     searchBar: {
@@ -68,13 +97,16 @@ const styles = StyleSheet.create({
     },
     searchFiltersContainer: {
         paddingHorizontal: 10,
-        paddingVertical: 10
+        paddingVertical: 10,
+        alignItems: "flex-start",
+        flexDirection: "row",
+        columnGap: 3
     },
     searchTag: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        columnGap: 6,
+        columnGap: 7,
         justifyContent: "space-between",
         alignSelf: "flex-start",
         backgroundColor: "orange",
@@ -86,6 +118,14 @@ const styles = StyleSheet.create({
     tagIcon: {
         width: 15,
         height: 15,
-        marginTop: 3
+        marginTop: 1,
+        borderRadius: 4,
+        backgroundColor: "gray"
+    },
+    eventsList: {
+        margin: 4
+    },
+    sectionDate: {
+        marginLeft: 2
     }
 })
